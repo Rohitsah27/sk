46bc6d0e16a68fc5337b4edfd27b68cb052e36ca
rohit27bmdu <rohitkr27.bmdu@gmail.com>
2025-05-03 18:43:26 +0530

    deploy in netlify

--- src/data/products.ts
+++ src/data/products.ts
@@ -17,6 +17,23 @@ export interface Product {
 }
 
 
+// export const fetchProducts = async (): Promise<Product[]> => {
+//   const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+
+//   const res = await fetch(`${baseUrl}/api/products`); // Ensure the API route exists and is correct
+//   if (!res.ok) {
+//     throw new Error(`Failed to fetch products: ${res.status} ${res.statusText}`);
+//   }
+//   const contentType = res.headers.get('Content-Type');
+//   if (contentType && contentType.includes('application/json')) {
+//     const data = await res.json();
+//     return data;
+//   } else {
+//     throw new Error('Invalid response format: Expected JSON');
+//   }
+// };
+
+
 export const fetchProducts = async (): Promise<Product[]> => {
   const baseUrl = 'https://sk-equipments.netlify.app'; // Hardcoded BASE_URL
 
